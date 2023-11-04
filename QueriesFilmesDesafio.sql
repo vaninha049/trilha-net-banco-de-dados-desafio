015ef0b6a61910f300af4fe4caccff6d60ef65f5
Vanessa Vieira <vaninha049@hotmail.com>
2023-11-04 19:18:41 -0300

    Ultima Atualizacao

--- QueriesFilmesDesafio.sql
+++ QueriesFilmesDesafio.sql
@@ -29,6 +29,56 @@
     order by Duracao
 
 -- 7 Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duração em ordem decrescente
+   
+   SELECT
+        Ano,
+        count(*) Quantidade 
+   FROM Filmes
+   GROUP BY Ano
+   ORDER BY Quantidade DESC
+
+ -- 8 Buscar os atores do genero masculino, retornando o primeiro nome, ultimo nome
+
+	select * from Atores
+	where Genero = 'M'
+
+-- 9 Buscar os atores do genero Feminino, retornando o primeiro nome, ultimo nome e ordenando pelo primeiroNome
+  
+  select * from Atores
+	where Genero = 'F'
+	order by PrimeiroNome
+
+-- 10 Buscar o nome do Filme e o gênero
+	
+	SELECT Nome, Genero
+	FROM FILMES INNER JOIN FilmesGenero ON
+	Filmes.Id = FilmesGenero.IdFilme
+	INNER JOIN Generos On
+	FilmesGenero.IdGenero = Generos.Id
+
+
+	
+	
+-- 11 Buscar o nome do filme e o gênero do tipo "mistério"
+ 
+	SELECT Nome, Genero
+	FROM FILMES INNER JOIN FilmesGenero ON
+	Filmes.Id = FilmesGenero.IdFilme
+	INNER JOIN Generos On
+	FilmesGenero.IdGenero = Generos.Id
+	WHERE Genero = 'mistério'
+
+
+-- 12 Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+	SELECT Nome, PrimeiroNome, UltimoNome, Papel
+	FROM ATORES INNER JOIN ElencoFilme ON
+	Atores.Id = ElencoFilme.IdAtor 
+	INNER JOIN Filmes ON 
+	ElencoFilme.IdFilme = Filmes.Id
+
+
+    
+	
 
 
  
